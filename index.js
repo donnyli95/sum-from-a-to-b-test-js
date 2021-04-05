25d9d22112c012587cd861bdc7c66eee034c67ea
donnyli95 <sz3li@uwaterloo.ca>
2021-04-05 23:12:01 +0000

    I kinda get it...

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  // base case
+  if (fromN === toN) {
+    return fromN;
+  }
+
+  // if fromN < toN, add all number fromN toN
+  return fromN + sum(fromN + 1, toN)
 }
 
 module.exports = sum;
+
+
+
